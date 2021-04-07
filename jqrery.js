f11af9f5e469d22a0ae31c3291c2bf4c1c152e61
alirouidi <48867962+alirouidi@users.noreply.github.com>
2021-04-07 12:00:11 +0100

    Update jqrery.js

--- jqrery.js
+++ jqrery.js
@@ -1,11 +1,11 @@
- $("#example, body").vegas({
-                                            delay: 2500,
-                                            timer: false,
-                                            shuffle: true,
-                                            timer: true,
-                                            transition: 'fade',
-                                            transitionDuration: 2000,
-                                            slides: [{
-                                                src: 'https://i.imgur.com/O59nYe5.png'
-                                            }, ]
-                                        });
+$("#example, body").vegas({
+delay: 2500,
+timer: false,
+shuffle: true,
+timer: true,
+transition: 'fade',
+transitionDuration: 2000,
+slides: [{
+src: 'https://i.imgur.com/O59nYe5.png'
+}, ]
+});
